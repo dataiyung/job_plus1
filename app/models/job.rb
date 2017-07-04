b3256f449049149b8e33489bd78a6a627e0b430c
dataiyung <dataiyung@gmail.com>
2017-07-04 18:10:29 +0800

    admin can see resume list

--- app/models/job.rb
+++ app/models/job.rb
@@ -8,11 +8,12 @@ def publish!
   self.is_hidden = false
   self.save
 end
+has_many :resumes  
 scope :published, ->{where(is_hidden: false)}
 scope :recent, ->{order('created_at DESC')}
 def hide!
   self.is_hidden = true
   self.save
-has_many :resumes  
+
 end
 end
